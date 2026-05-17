94740993eb2b77bf407a329d84a44149b01972b9
blackKittys3890 <ohnezahn@sudi.de>
2026-05-17 16:01:47 +0200

    Implement basic plugin structure with onEnable and onDisable logging

--- src/main/kotlin/io/github/black_Kittys22/anarchie/Main.kt
+++ src/main/kotlin/io/github/black_Kittys22/anarchie/Main.kt
@@ -1,4 +1,16 @@
 package io.github.black_Kittys22.anarchie
 
-class Main {
+import org.bukkit.event.Listener
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin(), Listener{
+
+    override fun onEnable() {
+        logger.info("[ANARCHIE] I'm awake :)")
+    }
+
+    override fun onDisable() {
+        logger.info("[ANARCHIE] I'm dead :(")
+    }
+
 }
